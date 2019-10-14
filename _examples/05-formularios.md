93f308297a738f821ce9b571141281a14bda4018
Talita Pagani <talita.cpb@gmail.com>
2019-10-13 21:28:29 -0300

    Form examples

--- _examples/05-formularios.md
+++ _examples/05-formularios.md
@@ -10,3 +10,66 @@ breadcrumb:
   - url: /exemplos
     title: Exemplos
 ---
+
+Formulários são as páginas mais propoensas a ocorrer erros de acessibilidade, embora eles sejam os elementos com maior quantidade de recursos para acessibilidade. Um HTML básico organizado de forma semântica já é o suficiente para eliminar grande parte dos problemas.
+
+## Um exemplo de formulário pouco acessível
+
+Faça o teste: navegue neste formulário com o uso de um leitor de telas.
+
+<form method="POST" action="#" class="wrongForm">
+  <input type="text" name="fullName" placeholder="Digite seu nome completo" required>
+  <input type="text" name="email" placeholder="Informe seu e-mail" required>
+  <input type="text" name="phone" placeholder="Informe seu telefone no seguinte formato: (XX) XXXXX-XXXX">
+  <button class="formButton" type="submit">Enviar</button>
+</form>
+
+## Corrigindo os problemas
+
+<form method="POST" action="#" class="rightForm">
+
+  <p>
+    <label for="fullName">Nome completo <span class="required">(obrigatório)</span>:</label>
+    <input type="text" name="fullName" id="fullName" required>
+  </p>
+
+  <p>
+    <label for="email">E-mail <span class="required">(obrigatório)</span>:</label>
+    <input type="email" name="email" id="email" required>
+  </p>
+  
+  <p>
+    <label for="phone">Telefone <span class="optional">(opcional)</span>:</label>
+    <input type="tel" name="phone" id="phone" aria-describedby="phoneTip">
+    <span class="fieldTip" id="phoneTip">Formato: (99) 99999-9999.</span>
+  </p>
+
+  <button class="formButton" id="formButton" type="submit">Finalizar cadastro</button>
+</form>
+
+## Melhorando com WAI-ARIA
+
+<form method="POST" action="#" class="rightForm rightFormARIA">
+
+  <output id="formMessage" role="alert" aria-live="assertive" tabindex="0"></output>
+
+  <p>
+    <label for="fullName1">Nome completo <span class="required">(obrigatório)</span>:</label>
+    <input type="text" name="fullName1" id="fullName1" required aria-invalid="false" aria-describedby="nameMessage">
+    <span class="fieldMessage" id="nameMessage"></span>
+  </p>
+
+  <p>
+    <label for="email1">E-mail <span class="required">(obrigatório)</span>:</label>
+    <input type="email1" name="email1" id="email1" required aria-invalid="false" aria-describedby="emailMessage">
+    <span class="fieldMessage" id="emailMessage"></span>
+  </p>
+  
+  <p>
+    <label for="phone1">Telefone <span class="optional">(opcional)</span>:</label>
+    <input type="tel" name="phone1" id="phone1" aria-describedby="phoneTip">
+    <span class="fieldTip" id="phoneTip">Formato: (99) 99999-9999.</span>
+  </p>
+
+  <button class="formButton" id="formButtonARIA" type="submit">Finalizar cadastro</button>
+</form>
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,116 @@
+"use strict";
 
+document.addEventListener("DOMContentLoaded", (e) => {
+
+  const paForm = {
+    regular: {
+      button: document.getElementById("formButton"),
+      inputs: document.querySelectorAll(".rightForm input"),
+      name: document.getElementById("fullName"),
+      email: document.getElementById("email"),
+      phone: document.getElementById("phone")
+    },
+    aria: {
+      button: document.getElementById("formButtonARIA"),
+      inputs: document.querySelectorAll(".rightFormARIA input"),
+      message: document.getElementById("formMessage"),
+      name: document.getElementById("fullName1"),
+      email: document.getElementById("email1"),
+      phone: document.getElementById("phone1"),
+      nameMessage: document.getElementById("nameMessage"),
+      emailMessage: document.getElementById("emailMessage")
+    }
+  }
+
+  function initRegularForm() {
+
+    paForm.regular.button.disabled = true;
+
+    paForm.regular.inputs.forEach((input) => {
+      input.addEventListener("blur", () => {
+
+        if (paForm.regular.name.value !== '' && paForm.regular.email.value !== '') {
+          paForm.regular.button.disabled = false;
+        } else {
+          paForm.regular.button.disabled = true;
+        }
+
+      });
+    });
+
+  }
+
+  function initARIAForm() {
+
+    paForm.aria.button.disabled = true;
+
+    paForm.aria.button.addEventListener("click", (e) => {
+      e.preventDefault();
+      ariaFormValidation();
+    });
+
+    paForm.aria.inputs.forEach((input) => {
+      input.addEventListener("blur", () => {
+
+        if (paForm.aria.name.value !== '' && paForm.aria.email.value !== '') {
+          paForm.aria.button.disabled = false;
+        } else {
+          paForm.aria.button.disabled = true;
+        }
+
+      });
+
+    });
+
+  }
+
+  function ariaFormValidation() {
+
+    let frm = paForm.aria,
+        message = frm.message,
+        nameMessage = frm.nameMessage,
+        emailMessage = frm.emailMessage;
+
+    message.innerHTML = "";
+    nameMessage.innerHTML = "";
+    emailMessage.innerHTML = "";
+
+    if(isValid(frm.name) && isValid(frm.email)) {
+      message.classList.remove("error");
+      message.classList.add("success");
+
+      message.innerHTML = "Cadastro realizado com sucesso!";
+    } else {
+      message.classList.remove("success");
+      message.classList.add("error");
+
+      message.innerHTML = "O formulário apresenta erros que impedem a finalização do seu cadastro. Confira se todos os campos obrigatórios foram preenchidos e tente novamente.";
+
+      if(!isValid(frm.name)) {
+        nameMessage.innerHTML = "O nome não pode ficar em branco.";
+      }
+
+      if(!isValid(frm.email)) {
+        emailMessage.innerHTML = "O e-mail não pode ficar em branco e deve ser um válido.";
+      }
+    }
+
+    message.focus();
+  }
+
+  function isValid(field) {
+
+    if(field.validity.valid) {
+      field.setAttribute("aria-invalid", "false");
+      return true;
+    } else {
+      field.setAttribute("aria-invalid", "true");
+      return false;
+    }
+
+  }
+
+  initRegularForm();
+  initARIAForm();
+
+});
